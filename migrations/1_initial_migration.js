459060a7a757667f1c5181d229f2fc4427896316
Ahmed <expresswork850@gmail.com>
2025-05-16 00:11:48 +0300

    deleting all files that related with blockchain

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-// to read compiled contract and interact with it 
-const productTracking = artifacts.require("MedicalProductTracking");
-
-module.exports = function (deployer) {
-    //Deploys the contract to the blockchain.
-    deployer.deploy(productTracking);
-};
--- routes/contractRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contractController = require('../controllers/contractController');
-
-router.get('/history/:serialNumber', contractController.getProductHistory);
-router.get('/status/:serialNumber', contractController.getProductStatus);
-router.post('/check', contractController.checkProduct);
-
-module.exports = router;
